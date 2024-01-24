f430a6b7dc0921691aeeb16086a8f02d5dd9fbcb
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-01-24 20:11:31 -0300

    feat: req 3

--- src/main/java/com/trybe/acc/java/Moto.java
+++ src/main/java/com/trybe/acc/java/Moto.java
@@ -9,5 +9,41 @@ public class Moto extends VeiculoBase {
   /**
    * Atributos e Métodos.
    */
+  private String tipoMoto;
 
+  public Moto(
+      String marca,
+      String modelo,
+      int ano, String
+      tipoCombustivel,
+      int capacidadeTanque,
+      String tipoMoto
+  ) {
+    super(marca, modelo, ano, tipoCombustivel, capacidadeTanque);
+    this.tipoMoto = tipoMoto;
+  }
+
+  @Override
+  public double calcularConsumoCombustivel(double distancia) {
+    return distancia / 18;
+  }
+
+  @Override
+  public void exibirInformacoes() {
+    System.out.println("Marca: " + getMarca());
+    System.out.println("Modelo: " + getModelo());
+    System.out.println("Ano: " + getAno());
+    System.out.println("Tipo de combustível: " + getTipoCombustivel());
+    System.out.println("Capacidade do tanque: " + getCapacidadeTanque() + " litros");
+    System.out.println("Tipo da moto: " + getTipoMoto());
+  }
+
+  //  getters e setters
+  public String getTipoMoto() {
+    return tipoMoto;
+  }
+
+  public void setTipoMoto(String tipoMoto) {
+    this.tipoMoto = tipoMoto;
+  }
 }
